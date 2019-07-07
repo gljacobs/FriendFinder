4479a08c6672cfc2aa3aa971c2b0721d17148d16
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-07-06 23:12:50 -0700

    add modal component to form when submitted

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArr = [
     {
         "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        "image": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
         "scores": [
             "5",
             "1",
@@ -17,7 +17,7 @@ var friendsArr = [
     },
     {
         "name": "Jacob Deming",
-        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "image": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
         "scores": [
             "4",
             "2",
@@ -33,7 +33,7 @@ var friendsArr = [
     },
     {
         "name": "Jeremiah Scanlon",
-        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "image": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
         "scores": [
             "5",
             "2",
@@ -49,7 +49,7 @@ var friendsArr = [
     },
     {
         "name": "Louis T. Delia",
-        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "image": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
         "scores": [
             "3",
             "3",
@@ -65,7 +65,7 @@ var friendsArr = [
     },
     {
         "name": "Lou Ritter",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        "image": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
         "scores": [
             "4",
             "3",
@@ -81,7 +81,7 @@ var friendsArr = [
     },
     {
         "name": "Jordan Biason",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+        "image": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
         "scores": [
             "4",
             "4",
